81f61873e9df92bae36d2502035fbfb82434daf9
francisco.gallegos.luque <franciscogallegosluque@gmail.com>
2025-08-20 19:51:44 -0300

    Initial Commit

---
+++ unifolio-mobile-ios/unifolio_mobile_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  unifolio_mobile_iosApp.swift
+//  unifolio-mobile-ios
+//
+//  Created by Francisco Manuel Gallegos Luque on 20/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct unifolio_mobile_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ unifolio-mobile-iosTests/unifolio_mobile_iosTests.swift
@@ -0,0 +1,17 @@
+//
+//  unifolio_mobile_iosTests.swift
+//  unifolio-mobile-iosTests
+//
+//  Created by Francisco Manuel Gallegos Luque on 20/08/2025.
+//
+
+import Testing
+@testable import unifolio_mobile_ios
+
+struct unifolio_mobile_iosTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
